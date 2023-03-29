52e27b5b10ca7d40176aca6831c538811a25f20f
sunahha <129029924+sunahha@users.noreply.github.com>
2023-03-29 09:46:47 +0900

    commit

--- inventory/src/main/java/labshopmonolith/domain/Inventory.java
+++ inventory/src/main/java/labshopmonolith/domain/Inventory.java
@@ -3,7 +3,7 @@ package labshopmonolith.domain;
 import java.util.Date;
 import java.util.List;
 import javax.persistence.*;
-import labshopmonolith.MonolithApplication;
+import labshopmonolith.InventoryApplication;
 import lombok.Data;
 
 @Entity
@@ -21,7 +21,7 @@ public class Inventory {
     public void onPostPersist() {}
 
     public static InventoryRepository repository() {
-        InventoryRepository inventoryRepository = MonolithApplication.applicationContext.getBean(
+        InventoryRepository inventoryRepository = InventoryApplication.applicationContext.getBean(
             InventoryRepository.class
         );
         return inventoryRepository;
